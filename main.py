c216888e8892e914ac56340e85cd5b959bdf8431
root <obamekevin@gmail.com>
2021-03-10 07:41:26 +0100

    Adding of tags for documentation of API. Creating a Customer schema in schemas.py. Creating of endpoints: - create_user (is empty for the moment) - get_user (is empty for the moment) Adding sqlalchemy_binds  for sqlite in database.py.

--- main.py
+++ main.py
@@ -3,16 +3,46 @@ from typing import List
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-import crud, models
+import crud, models, schemas
 from database import SessionLocal, engine
 
 #Permet de se passer de Pydantic. On récupère les objets en les formattant directement en JSON
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse, RedirectResponse
 
+from pydantic import BaseModel
+
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+
+tags_metadata = [
+{
+	"name":"customers", 
+	"description": "Retrieve a list of the customers from Mysql database.",
+},
+{
+	"name":"customer_per_id",
+	"description":"Retrive a specific customer with the id in parameter.",
+	
+},
+
+{
+	"name":"products",
+	"description":"Retrive a list of products from Mysql Database.",
+}, 
+{
+	"name":"product_per_id",
+	"description":"Retrive a specific product with the id in parameter.",
+	
+},
+]
+
+app = FastAPI(title="API DB ", 
+	description="API Project for get a data from Mysql and MongoDB databases.",
+	version= "0.1", 
+	openapi_tags= tags_metadata)
+
+
 
 #Dépendences
 def get_db():
@@ -44,14 +74,30 @@ def get_user():
 
 
 # endpoint qui renvoie 10 customers
-@app.get("/v1/bike/customers")
+@app.get("/v1/bike/customers", response_model=schemas.Customer,tags=["customers"])
 def list_customers(db: Session = Depends(get_db), skip: int = 0, limit: int = 10):
+
+	"""
+	Retrieve a list of Customers:
+
+	- **customer_id**: id of each customer, is unique
+	- **first_name**: first name of cutomer
+	- **last_name**: last name of customer
+	- **phone**: phone of the customer 
+	- **email**: email of the customer
+	- **street**: name of street in a city where the custoemr living
+	- **city**: city where the cutomer living
+	- **state**: state where the custoemr living
+	- **zip_code**: zip code of city
+	"""
+
+
 	customers = crud.get_customers(db, skip=skip, limit=limit)
 	json_compatible_customers_data = jsonable_encoder(customers)
 	return JSONResponse(content=json_compatible_customers_data)
 
 #endpoint qui renvoie le customer selon le custoemr_id fourni
-@app.get("/v1/bike/customers/{customer_id}")
+@app.get("/v1/bike/customers/{customer_id}", tags=["customer_per_id"])
 def read_customer(customer_id: int, db: Session = Depends(get_db)):
 	db_customer = crud.get_customer(db, customer_id = customer_id)
 	if db_customer is None:
@@ -60,14 +106,14 @@ def read_customer(customer_id: int, db: Session = Depends(get_db)):
 	return JSONResponse(content=json_compatible_customer_id_data)
 
 # endpoint qui renvoie 10 produits
-@app.get("/v1/bike/products")
+@app.get("/v1/bike/products", tags=["products"])
 def list_products(db: Session = Depends(get_db), skip: int = 0, limit: int = 10):
 	products = crud.get_products(db, skip=skip, limit=limit)
 	json_compatible_products_data = jsonable_encoder(products)
 	return JSONResponse(content=json_compatible_products_data)
 
 #endpoint qui renvoie le produit selon le product_id fourni
-@app.get("/v1/bike/products/{product_id}")
+@app.get("/v1/bike/products/{product_id}", tags=["product_per_id"])
 def read_product(product_id: int, db: Session = Depends(get_db)):
 	db_product = crud.get_product(db, product_id = product_id)
 	if db_product is None:
--- schemas.py
+++ schemas.py
@@ -6,6 +6,16 @@ from pydantic import BaseModel
 
 class Customer(BaseModel):
 
+	customer_id: int 
+	first_name: Optional[str] = None
+	last_name: Optional[str] = None
+	phone: Optional[str] = None
+	email: Optional[str] = None
+	street: Optional[str] = None
+	city: Optional[str] = None
+	state: Optional[str] = None
+	zip_code: Optional[str] = None
+
 #autorise l'application à récupérer l'objet ORM et de le transformer en objet automatiquement
 
 	class Config:
